85d6edb3145ee66d8177aaf2cf84a265674d7958
Tim Robillard <robillt@algaonquincollege.com>
2024-07-25 23:57:36 -0400

    add appUrl to env

--- views/dashboard/me_templ.go
+++ views/dashboard/me_templ.go
@@ -11,6 +11,8 @@ import "io"
 import "bytes"
 
 import (
+	"os"
+
 	"github.com/TimRobillard/handicap_tracker/store"
 	"github.com/TimRobillard/handicap_tracker/views/components"
 )
@@ -51,7 +53,7 @@ func Me(u *store.UIUser, rounds [20]*store.CalcRound) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(u.Username)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/dashboard/me.templ`, Line: 40, Col: 63}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/dashboard/me.templ`, Line: 42, Col: 63}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
@@ -64,7 +66,7 @@ func Me(u *store.UIUser, rounds [20]*store.CalcRound) templ.Component {
 		var templ_7745c5c3_Var3 string
 		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(u.LastScore)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/dashboard/me.templ`, Line: 41, Col: 69}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/dashboard/me.templ`, Line: 43, Col: 69}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 		if templ_7745c5c3_Err != nil {
@@ -82,7 +84,20 @@ func Me(u *store.UIUser, rounds [20]*store.CalcRound) templ.Component {
 				}
 			}
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</ul></div></div></div><div class=\"bg-white rounded-3xl col-span-3 row-span-2 p-4\"><span class=\"md:flex-1 font-semibold text-lg\">Indx</span><div class=\"flex items-center justify-center h-full\"><div id=\"chart-spinner\" class=\"animate-spin\"><i class=\"fa-solid fa-spinner text-4xl text-green-800\"></i></div><canvas id=\"myChart\" class=\"hidden\"></canvas><span id=\"chart-no-data\" class=\"hidden text-lg text-slate-500 text-center\">No Data Found</span></div></div></div></section><div class=\"hidden text-green-400\">generate class</div></main></body><script>\n\t\t\tdocument.addEventListener('DOMContentLoaded', \n\t\t\tasync () => {\n\t\t\t\ttry {\n\t\t\t\t\tconst response = await fetch('http://127.0.0.1:7331/dashboard/chart/me', {\n\t\t\t\t\t\theaders: {\n\t\t\t\t\t\t\t\"Accept\": \"application/json\"\n\t\t\t\t\t\t}\n\t\t\t\t\t});\n\t\t\t\t\tconst data = await response.json();\n\t\t\t\t\tif (data && data.data.length) {\n\t\t\t\t\tconst ctx = document.getElementById('myChart');\n\t\t\t\t\tnew Chart(ctx, {\n\t\t\t\t\t\ttype: 'line',\n\t\t\t\t\t\tdata: {\n\t\t\t\t\t\t\tlabels: data.labels,\n\t\t\t\t\t\t\tdatasets: [{\n\t\t\t\t\t\t\t\tlabel: '',\n\t\t\t\t\t\t\t\tdata: data.data,\n\t\t\t\t\t\t\t\tborderWidth: 2,\n\t\t\t\t\t\t\t}]\n\t\t\t\t\t\t},\n\t\t\t\t\t\tresponsive: true,\n\t\t\t\t\t\toptions: {\n\t\t\t\t\t\t\tfill: {\n\t\t\t\t\t\t\t\ttarget: 'origin',\n\t\t\t\t\t\t\t\tabove: 'rgb(22, 101, 52)',\n\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\tpointBackgroundColor: '#F2F2F2',\n\t\t\t\t\t\t\tpointBorderColor: '#333',\n\t\t\t\t\t\t\tpointRadius: 4,\n\t\t\t\t\t\t\tborderColor: 'rgb(22, 101, 52)',\n\t\t\t\t\t\t\tresponsive: true,\n\t\t\t\t\t\t\tscales: {\n\t\t\t\t\t\t\t\ty: {\n\t\t\t\t\t\t\t\t\tbeginAtZero: true,\n\t\t\t\t\t\t\t\t\tmin: data.min, max: data.max\n\t\t\t\t\t\t\t\t}\n\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\telements: {\n\t\t\t\t\t\t\t\tline: {\n\t\t\t\t\t\t\t\ttension: 0.2\n\t\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\tlayout: {\n\t\t\t\t\t\t\t\tpadding: 20\n\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\tplugins: {\n\t\t\t\t\t\t\t\tlegend: {\n\t\t\t\t\t\t\t\t\tdisplay: false,\n\t\t\t\t\t\t\t\t}\n\t\t\t\t\t\t\t}\n\t\t\t\t\t\t}\n\t\t\t\t\t});\n\t\t\t\t\tctx.classList.remove('hidden')\n\t\t\t\t\t} else {\n\t\t\t\t\t\tdocument.getElementById('chart-no-data').classList.remove('hidden')\n\t\t\t\t\t}\n\t\t\t\t} catch(e) {\n\t\t\t\t\tconsole.log(e)\n\t\t\t\t\t\tdocument.getElementById('chart-no-data').classList.remove('hidden')\n\t\t\t\t} finally {\n\t\t\t\t\tdocument.getElementById('chart-spinner').remove()\n\t\t\t\t}\n\t\t\t});\n\t\t\t// TODO move to score form\n\t\t\t// const scoreForm = document.getElementById('score-form');\n\t\t\t// const scoreInputs = document.querySelectorAll('#score-form input');\n\t\t\t// const scoreOut = document.getElementById('score-out');\n\t\t\t// const scoreIn = document.getElementById('score-in');\n\t\t\t// const scoreTot = document.getElementById('score-tot');\n\t\t\t// const saveTot = document.getElementById('save-tot');\n\n\t\t\t// scoreForm.addEventListener('input', calculateScore)\n\n\t\t\t// function calculateScore() {\n\t\t\t// \tlet outVal = 0;\n\t\t\t// \tlet inVal = 0;\n\t\t\t// \tscoreInputs.forEach(input => {\n\t\t\t// \t\tconst hole = Number(input.id.split('-')[1]);\n\t\t\t// \t\tconst value = Number(input.value);\n\t\t\t// \t\tif (hole < 10) {\n\t\t\t// \t\t\toutVal += value;\n\t\t\t// \t\t\treturn;\n\t\t\t// \t\t}\n\t\t\t// \t\tinVal += value;\n\t\t\t// \t})\n\t\t\t// \tscoreOut.innerHTML = outVal;\n\t\t\t// \tscoreIn.innerHTML = inVal;\n\t\t\t// \tscoreTot.innerHTML = outVal + inVal;\n\t\t\t// \tsaveTot.innerHTML = outVal + inVal;\n\t\t\t// }\n\t\t</script></html>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</ul></div></div></div><div class=\"bg-white rounded-3xl col-span-3 row-span-2 p-4\"><span class=\"md:flex-1 font-semibold text-lg\">Indx</span><div class=\"flex items-center justify-center h-full\"><div id=\"chart-spinner\" class=\"animate-spin\"><i class=\"fa-solid fa-spinner text-4xl text-green-800\"></i></div><canvas id=\"myChart\" class=\"hidden\"></canvas><span id=\"chart-no-data\" class=\"hidden text-lg text-slate-500 text-center\">No Data Found</span></div></div></div></section><div class=\"hidden text-green-400\">generate class</div></main><div id=\"app-url\" class=\"hidden\" app-url=\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var4 string
+		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(os.Getenv("APP_URL"))
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/dashboard/me.templ`, Line: 74, Col: 65}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"></div></body><script>\n\t\t const appUrl = document.getElementById('app-url').getAttribute('app-url');\n\t\t\tdocument.addEventListener('DOMContentLoaded', \n\t\t\tasync () => {\n\t\t\t\ttry {\n\t\t\t\t\tconst response = await fetch(`${appUrl}/dashboard/chart/me`);\n\t\t\t\t\tconst data = await response.json();\n\t\t\t\t\tif (data && data.data.length) {\n\t\t\t\t\tconst ctx = document.getElementById('myChart');\n\t\t\t\t\tnew Chart(ctx, {\n\t\t\t\t\t\ttype: 'line',\n\t\t\t\t\t\tdata: {\n\t\t\t\t\t\t\tlabels: data.labels,\n\t\t\t\t\t\t\tdatasets: [{\n\t\t\t\t\t\t\t\tlabel: '',\n\t\t\t\t\t\t\t\tdata: data.data,\n\t\t\t\t\t\t\t\tborderWidth: 2,\n\t\t\t\t\t\t\t}]\n\t\t\t\t\t\t},\n\t\t\t\t\t\tresponsive: true,\n\t\t\t\t\t\toptions: {\n\t\t\t\t\t\t\tfill: {\n\t\t\t\t\t\t\t\ttarget: 'origin',\n\t\t\t\t\t\t\t\tabove: 'rgb(22, 101, 52)',\n\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\tpointBackgroundColor: '#F2F2F2',\n\t\t\t\t\t\t\tpointBorderColor: '#333',\n\t\t\t\t\t\t\tpointRadius: 4,\n\t\t\t\t\t\t\tborderColor: 'rgb(22, 101, 52)',\n\t\t\t\t\t\t\tresponsive: true,\n\t\t\t\t\t\t\tscales: {\n\t\t\t\t\t\t\t\ty: {\n\t\t\t\t\t\t\t\t\tbeginAtZero: true,\n\t\t\t\t\t\t\t\t\tmin: data.min, max: data.max\n\t\t\t\t\t\t\t\t}\n\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\telements: {\n\t\t\t\t\t\t\t\tline: {\n\t\t\t\t\t\t\t\ttension: 0.2\n\t\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\tlayout: {\n\t\t\t\t\t\t\t\tpadding: 20\n\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\tplugins: {\n\t\t\t\t\t\t\t\tlegend: {\n\t\t\t\t\t\t\t\t\tdisplay: false,\n\t\t\t\t\t\t\t\t}\n\t\t\t\t\t\t\t}\n\t\t\t\t\t\t}\n\t\t\t\t\t});\n\t\t\t\t\tctx.classList.remove('hidden')\n\t\t\t\t\t} else {\n\t\t\t\t\t\tdocument.getElementById('chart-no-data').classList.remove('hidden')\n\t\t\t\t\t}\n\t\t\t\t} catch(e) {\n\t\t\t\t\tconsole.log(e)\n\t\t\t\t\t\tdocument.getElementById('chart-no-data').classList.remove('hidden')\n\t\t\t\t} finally {\n\t\t\t\t\tdocument.getElementById('chart-spinner').remove()\n\t\t\t\t}\n\t\t\t});\n\t\t\t// TODO move to score form\n\t\t\t// const scoreForm = document.getElementById('score-form');\n\t\t\t// const scoreInputs = document.querySelectorAll('#score-form input');\n\t\t\t// const scoreOut = document.getElementById('score-out');\n\t\t\t// const scoreIn = document.getElementById('score-in');\n\t\t\t// const scoreTot = document.getElementById('score-tot');\n\t\t\t// const saveTot = document.getElementById('save-tot');\n\n\t\t\t// scoreForm.addEventListener('input', calculateScore)\n\n\t\t\t// function calculateScore() {\n\t\t\t// \tlet outVal = 0;\n\t\t\t// \tlet inVal = 0;\n\t\t\t// \tscoreInputs.forEach(input => {\n\t\t\t// \t\tconst hole = Number(input.id.split('-')[1]);\n\t\t\t// \t\tconst value = Number(input.value);\n\t\t\t// \t\tif (hole < 10) {\n\t\t\t// \t\t\toutVal += value;\n\t\t\t// \t\t\treturn;\n\t\t\t// \t\t}\n\t\t\t// \t\tinVal += value;\n\t\t\t// \t})\n\t\t\t// \tscoreOut.innerHTML = outVal;\n\t\t\t// \tscoreIn.innerHTML = inVal;\n\t\t\t// \tscoreTot.innerHTML = outVal + inVal;\n\t\t\t// \tsaveTot.innerHTML = outVal + inVal;\n\t\t\t// }\n\t\t</script></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
